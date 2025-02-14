c7addcb59c02c75de6d2674b3711ec853951013e
Sahil saw <sahilsaw23@gmail.com>
2025-02-15 02:53:22 +0530

    daisyui-commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,10 @@ connectDB();
 const PORT= process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use('/api/auth',authRouter);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { axiosInstance } from './libs/axios.lib';
+
+import './app.css';
 
 function App() {
+  async function callfun() {
+    try {
+      const response = await axiosInstance.post('/auth/login', {
+        password: 'omarora123',
+        email: 'om@gmail.com',
+      });
+
+      console.log('Login Successful:', response.data); 
+    } catch (error) {
+      console.error('Login Failed:', error.response ? error.response.data : error.message);
+    }
+  }
+
   return (
-    <>
-    </>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-base-200">
+      <button className="btn btn-primary" onClick={callfun}>Click Me</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
